4e79edc44f67e90eb94800db473896d9643dd002
shivdotsol <defi.shiv@gmail.com>
2026-01-05 10:22:32 +0530

    p2c-on-action-progress-bar

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
 
-export default class P2cParentComponent extends LightningElement {}
+export default class P2cParentComponent extends LightningElement {
+  @track percentage = 10;
+  changeHandler(event) {
+    this.percentage = event.target.value;
+  }
+}
